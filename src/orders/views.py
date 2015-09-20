de4f40fc0052032dc8f6bb166586c14bc78e006e
Coding for Entrepreneurs <codingforentrepreneurs@gmail.com>
2015-09-20 16:43:26 -0700

    18 - User Checkout Part 2

--- src/orders/views.py
+++ src/orders/views.py
@@ -1,4 +1,5 @@
 from django.contrib import messages
+from django.contrib.auth import get_user_model
 from django.http import Http404
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, FormView
@@ -17,15 +18,50 @@ from .forms import AddressForm, UserAddressForm
 from .mixins import CartOrderMixin, LoginRequiredMixin
 from .models import UserAddress, UserCheckout, Order
 
+User = get_user_model()
 
 class UserCheckoutMixin(object):
+
+	def user_failure(self, message=None):
+		data = {
+			"message": "There was an error. Please try again",
+			"success": False
+		}
+		if message:
+			data["message"] = message
+		return data
+
+
 	def get_checkout_data(self, user=None, email=None):
+		if email and not user:
+			user_exists = User.objects.filter(email=email).count()
+			if user_exists != 0:
+				return self.user_failure(message="This user already exists, please login.")
+
 		data = {}
-		if user.is_authenticated():
-			user_checkout = UserCheckout.objects.get_or_create(user=user)[0] #(instance, created)
+		if user and not email:
+			if user.is_authenticated():
+				user_checkout = UserCheckout.objects.get_or_create(user=user, email=user.email)[0] #(instance, created)
+				data["token"] = user_checkout.get_client_token()
+				data["braintree_id"] = user_checkout.get_braintree_id
+				data["user_checkout_id"] = user_checkout.id
+			else:
+				#return error
+				pass
+		elif not user and email:
+			user_checkout = UserCheckout.objects.get_or_create(email=email)[0] #(instance, created)
+			
+		elif user and email:
+			user_checkout = False
+		else:
+			user_checkout = False
+
+		if user_checkout:
 			data["token"] = user_checkout.get_client_token()
 			data["braintree_id"] = user_checkout.get_braintree_id
 			data["user_checkout_id"] = user_checkout.id
+			data["success"]= True
+
 		return data
 
 
@@ -33,6 +69,13 @@ class UserCheckoutAPI(UserCheckoutMixin, APIView):
 	permission_classes = [AllowAny]
 	def get(self, request, format=None):
 		data = self.get_checkout_data(user=request.user)
+		return Response(data)
+
+	def post(self, request, format=None):
+		data = {}
+		email = request.data.get("email")
+		if email:
+			data = self.get_checkout_data(email=email)
 		return Response(data)
 
 
