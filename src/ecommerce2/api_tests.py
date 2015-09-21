6418b72838e251f763efd426e615f393973bc7ed
Coding for Entrepreneurs <codingforentrepreneurs@gmail.com>
2015-09-21 00:26:56 -0700

    21 - Testing JWT Tokens with Python Requests

---
+++ src/ecommerce2/api_tests.py
@@ -0,0 +1,37 @@
+import requests
+
+
+
+base_url = "http://127.0.0.1:8000/api/"
+
+login_url = base_url + "auth/token/"
+
+products_url = base_url + "products/"
+
+
+#requests.get
+#requests.post(login_url, data=None, headers=None, params=None)
+
+
+data = {
+	"username": "jmitchel3",
+	"password": "123"
+}
+login_r = requests.post(login_url, data=data)
+login_r.text
+json_data = login_r.json()
+
+import json
+
+print(json.dumps(json_data, indent=2))
+
+token = json_data["token"]
+print token
+
+headers = {
+	"Authorization": "JWT %s" %(token),
+}
+
+p_r = requests.get(products_url, headers=headers)
+print p_r.text
+print(json.dumps(p_r.json(), indent=2))
