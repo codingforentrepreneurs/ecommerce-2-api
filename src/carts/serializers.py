3ab5e9159835fa297063e4278916cef5c4e1c77c
Coding for Entrepreneurs <codingforentrepreneurs@gmail.com>
2015-09-23 14:43:37 -0700

    39 - Serializer Validation

--- src/carts/serializers.py
+++ src/carts/serializers.py
@@ -1,11 +1,12 @@
 from rest_framework import serializers
 
 
-
+from orders.models import UserAddress, UserCheckout
 from products.models import Variation
 
-from .models import CartItem
 
+from .models import CartItem, Cart
+from .mixins import TokenMixin
 
 """
 
@@ -17,7 +18,7 @@ from .models import CartItem
 }
 
 """
-class CheckoutSerializer(serializers.Serializer):
+class CheckoutSerializer(TokenMixin, serializers.Serializer):
 	checkout_token = serializers.CharField()
 	user_checkout_id =serializers.IntegerField(required=False)
 	billing_address = serializers.IntegerField()
@@ -25,12 +26,54 @@ class CheckoutSerializer(serializers.Serializer):
 	cart_token = serializers.CharField()
 	cart_id = serializers.IntegerField(required=False)
 
+	def validate(self, data):
+		checkout_token = data.get("checkout_token")
+		billing_address = data.get("billing_address")
+		shipping_address = data.get("shipping_address")
+		cart_token = data.get("cart_token")
 
-	def validate_checkout_token(self, value):
-		print type(value)
-		if type(value) == type(str()):
-			return value
-		raise serializers.ValidationError("This is not a valid token.")
+		cart_token_data = self.parse_token(cart_token)
+		cart_id = cart_token_data.get("cart_id")
+		print cart_token_data
+
+
+		checkout_data = self.parse_token(checkout_token)
+		user_checkout_id = checkout_data.get("user_checkout_id")
+		print checkout_data
+
+
+		try:
+			cart_obj = Cart.objects.get(id=int(cart_id))
+			data["cart_id"] = cart_obj.id
+		except:
+			raise serializers.ValidationError("This is not a valid cart")
+
+		try:
+			user_checkout = UserCheckout.objects.get(id=int(user_checkout_id))
+			data["user_checkout_id"] = user_checkout.id
+		except:
+			raise serializers.ValidationError("This is not a valid user")
+
+
+		try:
+			billing_obj = UserAddress.objects.get(user__id=int(user_checkout_id), id=int(billing_address))
+		except:
+			raise serializers.ValidationError("This is not a valid address for this user")
+
+		try:
+			shipping_obj = UserAddress.objects.get(user__id=int(user_checkout_id), id=int(shipping_address))
+		except:
+			raise serializers.ValidationError("This is not a valid address for this user")
+
+		return data
+
+	# def validate_<fieldname>(self, value):
+	#   	return value
+	# def validate_checkout_token(self, value):
+	# 	print type(value)
+	# 	if type(value) == type(str()):
+	# 		return value
+	# 	raise serializers.ValidationError("This is not a valid token.")
 
 
 
