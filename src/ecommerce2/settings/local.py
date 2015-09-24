5b6d9de8e3f58c2c93638bc88fd3b257b8623ca0
Coding for Entrepreneurs <codingforentrepreneurs@gmail.com>
2015-09-24 15:02:20 -0700

    46 - Django CORS Headers.mp4

--- src/ecommerce2/settings/local.py
+++ src/ecommerce2/settings/local.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     #third party apps
+    'corsheaders',
     'crispy_forms',
     'django_filters',
     'registration',
@@ -69,6 +70,7 @@ INSTALLED_APPS = (
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -77,6 +79,15 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
 )
+
+# CORS_ORIGIN_WHITELIST = (
+#     'ang.heroku.com'
+# )
+
+# CORS_ORIGIN_REGEX_WHITELIST = ('^(https?://)?(\w+\.)?teamcfe\.com$', )
+CORS_URLS_REGEX = r'^/api/.*$'
+
+
 
 ROOT_URLCONF = 'ecommerce2.urls'
 
